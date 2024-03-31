e9048023dab2917c09a3232b2ac1a73b8e51e2c4
dazhi <dazhi1@sohu.com>
2024-03-31 14:21:52 +0800

    Initial Commit

---
+++ DrinkWater/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  DrinkWater
+//
+//  Created by 宋成志 on 2024/3/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var isWatered = false
+    
+    var body: some View {
+        
+        VStack {
+            ZStack{
+                Image("background")
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+
+                
+                Image(systemName: isWatered ? "crown.fill":"eyes")
+                    .foregroundColor(isWatered ? /*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/:.black)
+                    .font(.system(size: 200))
+                    
+            }
+            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            .frame(minHeight: 650)
+            
+            Divider()
+            
+            Toggle(isOn: $isWatered) {
+                Label("喝水了吗", systemImage: "drop.fill")
+            }
+            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            
+        }
+        .padding()
+        
+    }
+    
+}
+
+
+
+
+
+#Preview {
+    ContentView()
+}
---
+++ DrinkWater/DrinkWaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkWaterApp.swift
+//  DrinkWater
+//
+//  Created by 宋成志 on 2024/3/30.
+//
+
+import SwiftUI
+
+@main
+struct DrinkWaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
